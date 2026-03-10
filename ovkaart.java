04666c6ffb5b7d621b364581599354ffbf0b133c
Mitsubi <agp.842007@gmail.com>
2026-03-10 11:10:05 +0100

    ovkaart update
    i discussed with my teacher how things work and changed the file name and added some comments that shows me where things will be added.

--- ovkaart.java
+++ ovkaart.java
@@ -1,5 +1,19 @@
 package oopKaart;
 
-public class ovkaart {
+public class OvKaart {
+	// Variabelen
+	private int kaartNummer;
+	private double saldo;
 
+	// Constructor
+	public OvKaart(int kaartNummer, double saldo) {
+		
+	}
+
+	// Functies
+	
+	public boolean inChecken() {
+		
+		return true;
+	}
 }
